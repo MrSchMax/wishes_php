503f00261dd58f0e6a3cad71dcd605707ed9620e
mr_schmax <mr.schmax@gmail.com>
2022-10-22 13:09:53 +0300

    исправлена валидация модели Gift

--- src/models/Gift.php
+++ src/models/Gift.php
@@ -9,7 +9,7 @@ class Gift  extends AbstractModelWithOwner
     protected const TABLE = 'gifts';
     protected const VALIDATORS = [
         'name' => 'required|alpha_spaces|min:2',
-        'product_price' => 'required|regex:/\d{1,10}(\.\d{1,2})?/',
+        'product_price' => 'required|regex:/^\d{1,10}(\.\d{1,2})?$/',
         'photo' => 'required|url',
         'link' => 'required|url',
         'userId' => 'required|integer',
