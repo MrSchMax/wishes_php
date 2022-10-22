cb1986832ad71c9708ac8598bded999846975a1a
mr_schmax <mr.schmax@gmail.com>
2022-10-22 12:25:30 +0300

    создана модель Gift

---
+++ src/models/Gift.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\models;
+
+use app\AbstractModelWithOwner;
+
+class Gift  extends AbstractModelWithOwner
+{
+    protected const TABLE = 'gifts';
+    protected const VALIDATORS = [
+        'name' => 'required|alpha_spaces|min:2',
+        'product_price' => 'required|regex:/\d{1,10}(\.\d{1,2})?/',
+        'photo' => 'required|url',
+        'link' => 'required|url',
+        'userId' => 'required|integer',
+    ];
+}
